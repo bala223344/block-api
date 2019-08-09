3b6876f3e070f53e3995743a14390f8d9d0979f6
mahima <manish.excel2011@gmail.com>
2019-08-09 16:15:53 +0530

    fixed

--- app/__init__.py
+++ app/__init__.py
@@ -33,7 +33,7 @@ def create_app(test_config=None):
     
 
     auto_fetch_scheduler = BackgroundScheduler()
-    auto_fetch_scheduler.add_job(auto_fetch, trigger='cron', day_of_week='mon-sat', hour=10,minute=41)
+    auto_fetch_scheduler.add_job(auto_fetch, trigger='cron', day_of_week='mon-sat', hour=10,minute=48)
     auto_fetch_scheduler.start()
         
 
