40aae9718c483bc60c1bac2f201d8b73b04b2935
mahima <manish.excel2011@gmail.com>
2019-08-21 16:16:06 +0530

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -54,7 +54,7 @@ def create_app(test_config=None):
     
     tx_notification_scheduler = BackgroundScheduler()
     #tx_notification_scheduler.add_job(tx_notification, trigger='cron', day_of_week='mon-sat', hour=11,minute=24)
-    tx_notification_scheduler.add_job(tx_notification, trigger='interval', seconds=3000000)
+    tx_notification_scheduler.add_job(tx_notification, trigger='interval', seconds=300)
     tx_notification_scheduler.start()
 
     risk_score_scheduler = BackgroundScheduler()
