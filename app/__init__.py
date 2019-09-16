2aa2dac169f78503194bd3bb241758142732268e
mahima <manish.excel2011@gmail.com>
2019-09-16 15:28:08 +0530

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -74,7 +74,7 @@ def create_app(test_config=None):
     risk_score_by_heist_scheduler.start()
     
     tx_notification_scheduler = BackgroundScheduler()
-    tx_notification_scheduler.add_job(tx_notification, trigger='cron', day_of_week='mon-sat', hour=9, minute=56)
+    tx_notification_scheduler.add_job(tx_notification, trigger='cron', day_of_week='mon-sat', hour=10, minute=5)
     #tx_notification_scheduler.add_job(tx_notification, trigger='interval', minutes=10)
     tx_notification_scheduler.start()
     #tx_notification_scheduler.add_job(tx_notification, trigger='interval', minutes=tx_notification_scheduler_minute)
