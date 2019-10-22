3e2e6ea238e331832ef4d2966477cd695c89b226
root <bala223344@gmail.com>
2019-10-22 11:11:35 +0000

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -124,13 +124,3 @@ def create_app(test_config=None):
         profile_risk_score_scheduler.shutdown()
         Top_user_percentage_scheduler.shutdown()
         invoice_notification_interval_scheduler.shutdown()
-
-<<<<<<< HEAD
-'''
-
-to = 0x94d0b8ccd2141a6969018be0bc25adc7ef91068c
-from =0xbcbf6ac5f9d4d5d35bac4029b73aa4b9ed5e8c0b
-1.005
-'''
-=======
->>>>>>> b9c60bde2f6067e5047a1834b50386d9f440f844
