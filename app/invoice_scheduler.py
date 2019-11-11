dc847818b83596e505155d25895f70b14008f252
mahima <manish.excel2011@gmail.com>
2019-11-11 16:08:50 +0530

    issues fixed

--- app/invoice_scheduler.py
+++ app/invoice_scheduler.py
@@ -92,9 +92,6 @@ def invoice_notification_interval():
 
 
 
-'<h3> You got a new transaction </h3><strong>Date:</strong> ' + str(dt_object) +' <div><strong>From:</strong> ' + str(frm_safename) + ' </div><strong>To:</strong> ' + str(to_safename) + ' </div><div><strong>Amount:</strong> ' + str(send_amount) + ' </div><div><strong>Tx_id:</strong> ' + str(tx_id) + ' </div><div><strong>Coin Type:</strong> ''ETH''  </div>'
-
-
 
 '''
 else:
