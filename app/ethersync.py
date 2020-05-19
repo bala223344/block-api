c10ccb4c180f3bba54fcdee71fafabdf7fb8af3b
pythexcel <pythexcel@excellencetechnologies.in>
2020-05-19 17:24:16 +0530

    fixed

--- app/ethersync.py
+++ app/ethersync.py
@@ -105,6 +105,7 @@ def EthSync():
         balance = response['result']
         amount_recived =""
         amount_sent =""
+        print(address)
         ret = mongo.db.dev_sws_history.update({
             "address":address            
         },{
